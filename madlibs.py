f279ef617430212625da4a4f0c53657809d04be7
Hackbright Student <info@hackbrightacademy.com>
2016-04-19 18:03:47 -0700

    Added checkbox feature

--- madlibs.py
+++ madlibs.py
@@ -48,17 +48,22 @@ def show_game_form():
     else:
         return render_template("game.html")
 
-@app.route('/madlib')
+@app.route('/madlib',methods=["POST"])
 def show_madlib():
     """Prints a madlib"""
 
-    person = request.args.get("person")
-    noun = request.args.get("noun")
-    color = request.args.get("color")
-    adj = request.args.get("adj")
+    person = request.form.get("person")
+    noun = request.form.get("noun")
+    color = request.form.get("color")
+    # adj1,adj2,adj3 = request.args.get("adj")
+    adj = request.form.getlist("adj")
+    # thing2 = request.args.get("adj")
+
+    # print thing 
+
 
     return render_template("madlib.html", person=person,
-        noun=noun,color=color,adj=adj)
+        noun=noun,color=color,adj1=adj[0], adj2=adj[1], adj3=adj[2])
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
